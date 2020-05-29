0d12512d93202319f627df43e3a05ebdbea393f2
votrungnam2013 <votrungnam2013@gmail.com>
2020-05-29 07:18:42 -0700

    add img folder and edit sign in page

--- JS/signin.js
+++ JS/signin.js
@@ -2,30 +2,32 @@ let submit = document.getElementById("btn-submit")
 let username = document.getElementById("txtUser");
 let password = document.getElementById("txtPwd");
 let checkwrg = document.getElementById("form-login")
-var users =[
-    {
-        username :"VoTrungNam",
-        password : "123"
-    },
-    {
-        username:"votrungnghia",
-        password:"456"
-    }
-]
-submit.addEventListener('click',()=>{
+let error = document.getElementById("err")
+let users,getdata;
+
+async function getData(url){
+    getdata = await fetch(url);
+    users = await getdata.json()
+}
+getData("https://5ed1024f4e6d7200163a045c.mockapi.io/api/c4e/users");
+
+submit.addEventListener('click',async ()=>{
     let check = false;
+    error.innerHTML = "<p>Sai tài khoản hoặc mật khẩu</p>";
     users.forEach(user => {
-        if(username.value === user.username && password.value === user.password){
-            window.open('home.html')
-            check=true;
-        }
+        if(user.username != undefined && user.password != undefined){
+         if(user.username.toLowerCase()==username.value.toLowerCase()&&user.password.toLowerCase()==password.value.toLowerCase()){
+             window.open('home.html','_self')
+             check=true;
+         }
+    }
     });
     if(check==false)
     {
-        
-        let para = document.createElement("p");
-        let node = document.createTextNode("Sai tài khoản hoặc mật khẩu");
-        para.appendChild(node);
-        checkwrg.appendChild(para);
+        error.style.display="block";
+        // let para = document.createElement("p");
+        // let node = document.createTextNode("Sai tài khoản hoặc mật khẩu");
+        // para.appendChild(node);
+        // checkwrg.appendChild(para);
     }
 })
